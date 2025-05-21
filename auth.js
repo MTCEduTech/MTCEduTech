7026a649bb1644d7b9402990d1959ee0ae4a5511
MTCEduTech <maithienchanh@gmail.com>
2025-05-21 10:08:06 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,9 @@
-(function checkAuth() {
-  if (localStorage.getItem("authenticated") !== "true") {
-    alert("Vui lòng đăng nhập trước khi truy cập.");
+(function () {
+  const isAuth = localStorage.getItem("authenticated") === "true";
+  if (!isAuth) {
+    alert("🔒 Vui lòng đăng nhập trước khi truy cập.");
     window.location.href = "index.html";
+    document.write("<h2 style='text-align:center;margin-top:50px;'>⏳ Đang chuyển hướng...</h2>");
   }
 })();
+
